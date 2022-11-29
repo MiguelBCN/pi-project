b1d34578f5dcb0492e89f44be12266256b506611
Miguel Huayllas <huayllasmiguel@gmail.com>
2022-11-29 01:20:36 +0100

    First attempt to parallelize

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,20 +1,59 @@
-object Hello {
 
-  def main(args: Array[String]): Unit = {
-    var k:Float = 1
-    var s:Float = 0
+// Scala code for thread creation by extending
+// the Thread class
+class Hello extends Thread {
+  override def run() {
+    // Displaying the thread that is running
+    println("Thread " + Thread.currentThread().getName() +
+      " is running.")
+  }
+}
 
-    var x: Unit = (0 to 100).foreach(i => {
-      if (i % 2 == 0 ) {
-        s += 4 / k
-      }
-      else {
-        s -= 4 / k
-      }
-      k += 2
-    })
+class Pi(var start: Int, var end: Int, var width: Double) extends Runnable {
+  var result = .0
+  var k: Double = 1
+  var mid:Double = 0
+  var height: Double = 0
 
-    println("Resultado: " + s )
+  override def run(): Unit = {
+    for (i <- start until end) {
+      mid = (i + 0.5) * width
+      height = 4.0 / (1.0 + mid * mid)
+      result += height
+      print(" " + result)
+
+    }
+    println(Thread.currentThread.getName + " result =" + result + " start " + start + " end " + end)
   }
 
+  def getResult: Double = result
+}
+
+// Creating object
+object Hello {
+  // Main method
+  def main(args: Array[String]) {
+    var area = .0
+    var width = .0
+    var sum = 0.0
+    val num_steps = 100
+    val num_threads = 10
+    val num_values_in_thread = num_steps / num_threads
+    var end = num_values_in_thread
+    var start = 0
+    width = 1.0 / num_steps.asInstanceOf[Double]
+
+    for (i <- 0 until num_threads) {
+      val p = new Pi(start, end, width)
+      val t = new Thread(p)
+      t.setName("thread " + i)
+      t.start()
+      t.join()
+      start = start + end
+      end = end + num_values_in_thread
+      sum += p.getResult
+    }
+    area = width * sum
+    println("Final result: " + sum)
+  }
 }
